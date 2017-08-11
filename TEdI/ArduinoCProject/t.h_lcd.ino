da746fea90825096487329b7481261bb841e11c2
dj020311 <30527721+dj020311@users.noreply.github.com>
2017-08-11 18:54:19 +0900

    temp/humid display
    fifth commit

---
+++ TEdI/ArduinoCProject/t.h_lcd.ino
@@ -0,0 +1,45 @@
+//YWROBOT
+//Compatible with the Arduino IDE 1.0
+//Library version:1.1
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+#include "DHT.h"
+#define DHTPIN 2 
+
+#define DHTTYPE DHT11   
+
+DHT dht(DHTPIN, DHTTYPE);
+
+LiquidCrystal_I2C lcd(0x3F,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+void setup()
+{
+  lcd.init();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+  dht.begin();
+
+}
+
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  float f = dht.readTemperature(true);
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  
+  float hif = dht.computeHeatIndex(f, h);
+  float hic = dht.computeHeatIndex(t, h, false);
+  
+  lcd.setCursor(0,0);
+  lcd.print("Temp: ");
+  lcd.print(t);
+  lcd.setCursor(0,1);
+  lcd.print("Humid: ");
+  lcd.print(h);
+}
